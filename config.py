559a529fdd9071e2786be94d39b86239aae8597f
tanzhunhui <141299794+tanzhunhui@users.noreply.github.com>
2023-08-29 16:33:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cdithv1rcja8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanzhunhui-employee"
+customregion = "us-east-1"
+
